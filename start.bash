79d7a8035f63d5834302cd967f96f0baf64d699c
Sergej Wouldnttell <rakoth.android@gmail.com>
2021-06-18 18:45:28 +0300

    Savegame

--- start.bash
+++ start.bash
@@ -2,6 +2,7 @@
 podman build -t myadom .
 podman run -it -v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v ${HOME}/GOG\ Games/:/GOG\ Games \
+	-v ${HOME}/.adom.data:/root/.adom.data \
 	-e DISPLAY=${DISPLAY} \
 	--device=/dev/dri:/dev/dri \
 	--device /dev/snd \
